e86984de6aef8e0f9c1d016fe5618940afc2957e
Andreas Lust <andreaslust@Admins-iMac-2.local>
2019-05-27 15:26:29 +0200

    initial bla

---
+++ schoolwebsite/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andreaslust/Projekte/Django_Backend/schoolwebsite/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
